7e2f84e89753f84d91d617fcd2d472de4e42845c
alyssalingad <alyssalingad@gmail.com>
2016-08-09 20:39:02 -0700

    added form validation and smooth navigation bar transitioning

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
 /**
  * Created by Alyssa on 8/7/2016.
  */
+
+$(document).ready(function() {
+    
+    // for navigation bar
+    $("a").click(function() {
+        var element_id = $(this).attr('class');
+        console.log(element_id);
+        $('html,body').animate(
+            {scrollTop: $(element_id).offset().top}, 'slow');
+
+        console.log("clickeeeeed");
+    });
+    
+    // for read on button
+    $("#read").click(function() {
+        var element_id = "#About";
+        $('html,body').animate(
+            {scrollTop: $(element_id).offset().top}, 'slow');
+
+    });
+    
+    
+    
+    
+});
+
